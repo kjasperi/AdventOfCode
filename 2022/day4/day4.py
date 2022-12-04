426ad2acfaa0c6908fa044bd18634712bef32dd7
Kristoffer Jäsperi <kristoffer.jasperi@gmail.com>
2022-12-04 12:39:36 +0100

    Minor cleanup of day4

--- 2022/day4/day4.py
+++ 2022/day4/day4.py
@@ -4,16 +4,11 @@ def get_lines_from_file():
     with open(sys.argv[1]) as inf:
         return inf.readlines()
 
-    
-
 lines = [line.rstrip() for line in get_lines_from_file()]
 
 def expand(section):
-    expand_set = set()
     start, stop = section.split('-')
-    for i in range(int(start), int(stop) + 1):
-        expand_set.add(i)
-    return expand_set
+    return {s for s in range(int(start), int(stop) + 1)}
 
 p1 = 0
 p2 = 0
@@ -22,13 +17,10 @@ for line in lines:
     group1, group2 = line.split(',')
     set1 = expand(group1)
     set2 = expand(group2)
-    if set1.issubset(set2):
-        p1 += 1
-    elif set2.issubset(set1):
+    if set1.issubset(set2) or set2.issubset(set1):
         p1 += 1
     
-    inter = set1.intersection(set2)
-    if len(inter):
+    if set1.intersection(set2):
         p2 += 1
 
 print(p1)
